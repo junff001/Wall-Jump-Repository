6e6ab7a1c5f48229de4312ef35d74a32eebd485c
JunseoJung <71419212+junff001@users.noreply.github.com>
2022-11-28 11:56:27 +0900

    올림

--- Wall Jump/Assets/Scritps/Manager/PoolManager.cs
+++ Wall Jump/Assets/Scritps/Manager/PoolManager.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class PoolManager : MonoSingleton<PoolManager>
 {
-    public PoolDictionary poolDictionary = new PoolDictionary();
+    public Grid grid;
+    public List<GameObject> pools;
+    private Dictionary<string, GameObject> poolDictionary = new Dictionary<string, GameObject>();
 
-    public void SpawnGrid(string tag, Vector2 position)
+    void Start()
     {
+        foreach (var pool in pools)
+        {
+            GameObject obj = Instantiate(pool);
+            obj.SetActive(false);
+            poolDictionary.Add(pool.name, obj);
+        }
 
+        SpawnStage("2 Stage", new Vector2(0, 26));
+    }
+
+    public void SpawnStage(string tag, Vector2 position)
+    {
+        if (poolDictionary.ContainsKey(tag))
+        {
+            poolDictionary[tag].SetActive(true);
+            poolDictionary[tag].transform.parent = grid.transform;
+            poolDictionary[tag].transform.position = position;
+        }
     }
 }
